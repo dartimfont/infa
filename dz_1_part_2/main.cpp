6d52554e8d4eb7541e254f0b48500f6eabbcb9ef
dartimfont <borakolt@gmail.com>
2022-06-01 00:42:31 +0300

    -

--- dz_1_part_2/main.cpp
+++ dz_1_part_2/main.cpp
@@ -1,96 +1,155 @@
 #include <iostream>
 
 struct Vector {
-    int dim;
+    int dim = 0;
     double* coords;
 };
 
-Vector* initVector (const int dim) {
+Vector* initVector(const int dim) {
     Vector* newVec = new Vector;
     newVec->dim = dim;
     newVec->coords = new double[dim];
     return newVec;
 }
 
-void inputVector (Vector** vec) {
+void inputVector(Vector** vec) {
     for (int i = 0; i < (*vec)->dim; i++) {
         std::cin >> (*vec)->coords[i];
     }
 }
 
-void outputVector (Vector* vec) {
+void outputVector(Vector* vec) {
     for (int i = 0; i < vec->dim; i++) {
         std::cout << vec->coords[i] << " ";
     }
     std::cout << std::endl;
 }
 
-void sumVec (Vector** res, Vector* vec_1, Vector* vec_2) {
-    for (int i = 0; i < (*res)->dim; i++) {
-        (*res)->coords[i] = vec_1->coords[i] + vec_2->coords[i];
+void sumVec(Vector* vec_1, Vector* vec_2) {
+    Vector* vecRes = initVector(vec_1->dim);
+    for (int i = 0; i < vecRes->dim; i++) {
+        vecRes->coords[i] = vec_1->coords[i] + vec_2->coords[i];
     }
+    std::cout << "vec 1 + vec 2 = ";
+    outputVector(vecRes);
 }
 
-void raznostVec (Vector** res, Vector* vec_1, Vector* vec_2) {
-    for (int i = 0; i < (*res)->dim; i++) {
-        (*res)->coords[i] = vec_1->coords[i] - vec_2->coords[i];
+void raznostVec(Vector* vec_1, Vector* vec_2) {
+    Vector* vecRes = initVector(vec_1->dim);
+    for (int i = 0; i < vecRes->dim; i++) {
+        vecRes->coords[i] = vec_1->coords[i] - vec_2->coords[i];
     }
+    std::cout << "vec 1 - vec 2 = ";
+    outputVector(vecRes);
 }
 
-void multVecForScale (Vector** res, Vector* vec, const double scale) {
-    for (int i = 0; i < (*res)->dim; i++) {
-        (*res)->coords[i] = scale * vec->coords[i];
+void multVecForScale(Vector* vec, const double scale) {
+    Vector* vecRes = initVector(vec->dim);
+    for (int i = 0; i < vecRes->dim; i++) {
+        vecRes->coords[i] = scale * vec->coords[i];
     }
+    std::cout << "vec * " << scale << " = ";
+    outputVector(vecRes);
 }
 
-bool equalVecs (Vector* vec_1, Vector* vec_2) {
+void equalVecs(Vector* vec_1, Vector* vec_2) {
+    bool flag = true;
     for (int i = 0; i < vec_1->dim; i++) {
         if (vec_1->coords[i] != vec_2->coords[i]) {
-            return false;
+            flag = false;
         }
     }
-    return true;
-}
 
-void test1() {
-    int dim;
-    std::cout << "Input dimmension of vectors: ";
-    std::cin >> dim;
-
-    Vector* vec_1 = initVector (dim);
-    Vector* vec_2 = initVector (dim);
-    Vector* vecRes = initVector (dim);
-
-    std::cout << "vec 1 = ";
-    inputVector (&vec_1);
- 
-    std::cout << "vec 2 = ";
-    inputVector (&vec_2);
-
-    sumVec (&vecRes, vec_1, vec_2);
-    std::cout << "vec 1 + vec 2 = ";
-    outputVector (vecRes);
-
-    raznostVec (&vecRes, vec_1, vec_2);
-    std::cout << "vec 1 - vec 2 = ";
-    outputVector (vecRes);
-
-    multVecForScale (&vecRes, vec_1, 5);
-    std::cout << "5 * vec 1 = ";
-    outputVector (vecRes);
-
-    if (equalVecs (vec_1, vec_2)) {
+    if (flag) {
         std::cout << "vec 1 equal vec2\n";
-    } else {
+    }
+    else {
         std::cout << "vec 1 not equal vec2\n";
     }
-
 }
 
-int main () {
-    test1();   
+void deleteVector(Vector** vec) {
+    if (*vec != NULL) {
+        delete[](*vec)->coords;
+        (*vec)->dim = 0;
+    }
+}
 
-    
+void init(Vector** vec_1, Vector** vec_2) {
+    int dim;
+    deleteVector(vec_1);
+    deleteVector(vec_2);
+    std::cout << "Input dimmension of vectors: ";
+    std::cin >> dim;
+    *vec_1 = initVector(dim);
+    *vec_2 = initVector(dim);
+    std::cout << "vec 1 = ";
+    inputVector(vec_1);
+    std::cout << "vec 2 = ";
+    inputVector(vec_2);
+}
 
+void forScale(Vector** vec) {
+    int dim, scale;
+    deleteVector(vec);
+    std::cout << "Input dimmension of vectors: ";
+    std::cin >> dim;
+    *vec = initVector(dim);
+    std::cout << "vec = ";
+    inputVector(vec);
+    std::cout << "Input scalar: ";
+    std::cin >> scale;
+    multVecForScale(*vec, scale);
+}
+
+void printMenu() {
+    std::cout   << "1. Input Vectors."              << std::endl
+                << "2. Sum."                        << std::endl
+                << "3. Raznost."                    << std::endl
+                << "4. Mult Vec For Scale."         << std::endl
+                << "5. Equal Vecs."                 << std::endl
+                << "6. Clear."                      << std::endl
+                << "7. Exit."                       << std::endl
+                << std::endl;
+}
+
+int main() {
+    int ch = -1;
+    Vector* vec_1 = NULL;
+    Vector* vec_2 = NULL;
+    Vector* vec = NULL;
+
+    printMenu();
+
+    while (ch != 7) {
+        std::cout << "Choose: ";
+        std::cin >> ch;
+        switch (ch) {
+            case 1: {
+                init(&vec_1, &vec_2);
+            } break;
+            case 2: {
+                sumVec(vec_1, vec_2);
+            } break;
+            case 3: {
+                raznostVec(vec_1, vec_2);
+            } break;
+            case 4: {
+                forScale(&vec);
+            } break;
+            case 5: {
+                equalVecs(vec_1, vec_2);
+            } break;
+            case 6: {
+                system("cls");
+                printMenu();
+            } break;
+        }
+    }
+
+    deleteVector(&vec_1);
+    deleteVector(&vec_2);
+    deleteVector(&vec);
+    system("pause");
     return 0;
 }
--- lab_11/main.cpp
+++ lab_11/main.cpp
@@ -2,60 +2,52 @@
 #include <cmath>
 #include <ctime>
 
-int main()
-{
+int main() {
 	srand((unsigned)time(NULL));
 	int length;
-	std::cout << "Input len of array (len % 2 = 1): "
-	do {
+	std::cout << "Input len of array (len is nechet): ";
+	std::cin >> length;
+	while (length % 2 == 0) {
 		std::cout << "Input correct length: ";
 		std::cin >> length;
-	} while (length % 2 == 0);
+	} 
 	int* array = new int[length];
 	std::cout << "Input array" << std::endl;
-	for (int i = 0; i < length; i++)
-	{
+	for (int i = 0; i < length; i++) {
 		*(array + i) = rand() % 101 - 50;
 		std::cout << *(array + i) << "\t";
 	}
-	std::cout << std::endl;
-	for (int i = 0; i < length; i++)
-	{
-		if (*(array + i) < 0)
-		{
-			std::cout << "Negative num " << *(array + i) << " addres " << (array + i) << std::endl;
+	std::cout << std::endl <<  "Negative num:\n";
+	for (int i = 0; i < length; i++) {
+		if (*(array + i) < 0) {
+			std::cout << *(array + i) 
+				<< " addres " << (array + i) << std::endl;
 		}
 	}
-	std::cout << "Average num " << *(array + length / 2) << " addres " << (array + length / 2) << std::endl;
+	std::cout << "Num in midle: " << *(array + length / 2) 
+		<< " addres " << (array + length / 2) << std::endl;
 	double sum = 0;
 	int counter = 0;
-	for (int i = 0; i < length; i++)
-	{
-		if (*(array + i) % 5 == 0)
-		{
+	for (int i = 0; i < length; i++) {
+		if (*(array + i) % 5 == 0) {
 			sum += *(array + i);
 		}
-		if (*(array + i) < 0)
-		{
+		if (*(array + i) < 0) {
 			counter++;
 		}
 	}
 	std::cout << "Sum kratnyh 5: " << sum << "\n"
 		<< "Quntity negativ num: " << counter << std::endl;
 
-	if (counter != 0 && sum != 0)
-	{
+	if (counter != 0 && sum != 0) {
 		std::cout << "Final Num: " << sum / counter << std::endl;
 		*(array + length / 2) = sum / counter;
 		std::cout << "Output array" << std::endl;
-		for (int i = 0; i < length; i++)
-		{
+		for (int i = 0; i < length; i++) {
 			std::cout << *(array + i) << "\t";
 		}
 		std::cout << std::endl;
-	}
-	else
-	{
+	} else {
 		std::cout << "Final num not found!" << std::endl;
 	}
 
