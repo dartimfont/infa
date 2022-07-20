bcc695bbcb5742b68cbb688592850b6e2ec80e48
dartimfont <borakolt@gmail.com>
2022-07-20 18:56:00 +0300

    delete some files

--- infa/dz1_part_1/module.hpp
+++
@@ -1,6 +0,0 @@
-#ifndef __MODULE_HPP__
-#define __MODULE_HPP__
-
-void deleteString (int** array, int& raw, int column, int index);
-
-#endif
--- infa/lab_14/Module.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef __MODULE_HPP__
-#define __MODULE_HPP__
-
-#include <iostream>
-
-void module(int** array, int length, int index);
-
-#endif
